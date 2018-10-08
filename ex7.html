c9c9d73316af94407e3a63401382f756e92c740d
root <root@Anthony.localdomain>
2018-10-08 10:10:53 +0200

    ex7

---
+++ ex7.html
@@ -0,0 +1,6 @@
+function calc(){
+var firstNumber = document.getElementById('firstNumber').value;
+var secondNumber = document.getElementById('secondNumber').value;
+result = firstNumber % secondNumber;
+alert(result);
+}
---
+++ ex7.js
@@ -0,0 +1,6 @@
+function calc(){
+var shoeSize = document.getElementById('shoeSize').value;
+var yearOfBirth = document.getElementById('yearOfBirth').value;
+result = (shoeSize *2 +5) * 50 - yearOfBirth + 1766;
+alert(result);
+}
